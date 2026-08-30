a7f289fd712fcbf01c23d466e31968ee098453ce
Moustafa <moustafa.mahmoudx7@gmail.com>
2026-08-30 12:49:02 +0300

    sec 10 T 1/2

--- task11/sec 10.js
+++ task11/sec 10.js
@@ -28,3 +28,35 @@ else {
         alert("Failed");
     }
 }
+
+
+ // Task2//
+
+function myLoop(start, end, skip, stop) {
+
+    if (
+        start !== undefined &&
+        end !== undefined &&
+        skip !== undefined &&
+        stop !== undefined
+    ) {
+
+        for (let i = start; i <= end; i++) {
+
+            if (i === skip) {
+                continue;
+            }
+
+            if (i === stop) {
+                break;
+            }
+
+            console.log(i);
+        }
+
+    } else {
+        alert("Please enter all numbers");
+    }
+}
+
+myLoop(1, 10, 3, 8);
